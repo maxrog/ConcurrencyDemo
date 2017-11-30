0a695df878550d7be92602ea3dfa77790092f3fa
Max Rogers <marfodub@gmail.com>
2017-11-30 15:17:42 -0600

    Concurrent download

--- ConcurrencyDemo/ConcurrencyDemo/Controllers/MultipleImagesViewController.swift
+++ ConcurrencyDemo/ConcurrencyDemo/Controllers/MultipleImagesViewController.swift
@@ -36,6 +36,28 @@ class MultipleImagesViewController: UIViewController {
         }
     }
     
+    // MARK: - Concurrent Download
+    
+    /// This offloads our downloading work to concurrent tasks to be run in the background on a default quality of service (priority) queue
+    /// It then switches back to the main queue before performing UI changes
+    /// ***NOTE***: The order of images being populated is not guaranteed
+    @IBAction func concurrentDownload(_ sender: UIButton) {
+        let defaultQueue = DispatchQueue.global(qos: .default)
+        
+        for (index, imageView) in self.imageViews.enumerated() {
+            defaultQueue.async {
+                let urlString = LargeImages.allImages[index].rawValue
+                guard let url = URL(string: urlString) else { return }
+                ImageDownloader.download(withURL: url, completionHandler: { (image) in
+                    DispatchQueue.main.async {
+                         imageView.image = image
+                    }
+                })
+            }
+        }
+    }
+    
+    
     
 }
 
